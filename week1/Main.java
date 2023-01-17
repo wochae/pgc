46b681a3c1857b04f554ac9e060acafcde1cb98a
wochae <officialwochae@gmail.com>
2023-01-17 22:59:04 +0900

    [feat] 가장 큰 수 배열과 스트링 빌더 for와 for-each 속도 비교

--- week1/Main.java
+++ week1/Main.java
@@ -1,7 +1,12 @@
 package week1;
 
+import week1.src.Sol2;
+
 public class Main {
     public static void main(String[] args) {
-
+        int[] arr = {6 ,10, 2};
+        Sol2 sol = new Sol2();
+        System.out.println(sol.solution(arr));
+        System.out.println(sol.solution(arr).equals("6210"));
     }
 }
---
+++ week1/src/Sol2.java
@@ -0,0 +1,44 @@
+package week1.src;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// 가장 큰 수
+public class Sol2 {
+    public String solution1(int[] numbers) {
+        String answer = "";
+        String[] numberToStr = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
+        Arrays.sort(numberToStr, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        });
+
+        if (numberToStr[0].equals("0")) return "0";
+
+        for(String str : numberToStr)
+            answer += str;
+        return answer;
+    }
+    public String solution(int[] numbers) {
+        StringBuilder answer = new StringBuilder();
+        boolean except = true;
+        String[] num = new String[numbers.length];
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = String.valueOf(numbers[i]);
+            if (numbers[i] != 0) except = false;
+        }
+        Arrays.sort(num, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        });
+        for (int i = 0; i < num.length; i++)
+            answer.append(num[i]);
+        if(except) answer = new StringBuilder("0");
+        return answer.toString();
+    }
+
+}
