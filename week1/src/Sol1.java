5fda83923788519d30975a878327400980209169
wochae <officialwochae@gmail.com>
2023-01-17 16:19:07 +0900

    [fix] 객체가 아닌 원시 자료형으로 시간 단축

--- week1/src/Sol1.java
+++ week1/src/Sol1.java
@@ -7,14 +7,12 @@ import java.util.Queue;
 public class Sol1 {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-
-        Queue<Integer> sq = new LinkedList<>();
-        for (int s : stations) sq.offer(s);
-
+        int si = 0;
         int pos = 1;
         while (pos <= n) {
-            if (!sq.isEmpty() && sq.peek() - w <= pos) {
-                pos = sq.poll() + w + 1;
+            if (si < stations.length && stations[si] - w <= pos) {
+                pos = stations[si] + w + 1;
+                si++;
             } else {
                 answer += 1;
                 pos += w * 2 + 1;
